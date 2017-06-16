c41f5046437c16eacf2c444aa9fce21a1400e694
steven2805 <stevenmac90@googlemail.com>
2017-06-16 22:00:53 +0100

    updated the dealer_controller to hand over delaer id

--- controllers/dealer_controller.rb
+++ controllers/dealer_controller.rb
@@ -1,8 +1,14 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/dealers.rb' )
+require_relative('..models/cars.rb')
 
 get '/dealers' do
   @dealers = Dealer.all()
   erb ( :"dealers/index" )
 end
+
+get '/dealers/:id/dealer_cars' do
+  @cars = Car.find_dealers_cars(params)
+end 
+
