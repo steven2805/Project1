e516e6b4146b486f6d54893c72454b885ade8121
steven2805 <stevenmac90@googlemail.com>
2017-06-17 14:52:25 +0100

    updating all tracked files

--- models/cars.rb
+++ models/cars.rb
@@ -3,7 +3,7 @@ require_relative('../db/sql_runner.rb')
 
 class Car
 
-  attr_reader(:id ,:make,:model,:category, :price,:dealer_id)
+  attr_reader(:id ,:make,:model,:category, :price, :available,:dealer_id)
 
   def initialize( options )
     @id = options['id'].to_i 
@@ -11,19 +11,20 @@ class Car
     @model = options['model']
     @category = options['category']
     @price = options['price']
+    @available = options['available']
     @dealer_id = options['dealer_id'].to_i
   end 
 
   def save()
-    sql = "INSERT INTO cars (make, model, category, price, dealer_id)
+    sql = "INSERT INTO cars (make, model, category, price, available, dealer_id)
     VALUES 
-    ('#{make}','#{model}','#{category}','#{price}','#{dealer_id}')
+    ('#{make}','#{model}','#{category}','#{price}','#{available}','#{dealer_id}')
     RETURNING *"
     result = SqlRunner.run(sql)
     @id = result.first()['id'].to_i 
   end
 
-  def self.find_dealer_cars(looking_for,id)
+  def self.find(looking_for,id)
     sql = "SELECT * FROM cars WHERE #{looking_for} = #{id}"
     results = SqlRunner.run( sql )
     return results.map {|car| Car.new(car)}
