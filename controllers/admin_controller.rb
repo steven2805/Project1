dc49f3c72b5c0c931fea5d45ffa1c90405abb984
steven2805 <stevenmac90@googlemail.com>
2017-06-17 15:54:21 +0100

    got the admin panel working and adding cars

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -5,3 +5,12 @@ require_relative( '../models/cars.rb')
 get '/admin' do
   erb ( :'admin/index')
 end
+
+get '/admin/new_car' do
+  @dealers = General.all('dealers','Dealer')
+  erb(:'admin/new_car')
+end
+
+post '/admin' do
+  Car.new(params).save
+end
